86888c995371121c3fafe93da63f897ac240c41f
mahmoudcod <mahmoud.pk@icloud.com>
2024-06-03 16:31:26 +0300

    restor v2

--- components/AppIcon.js
+++ components/AppIcon.js
@@ -1,13 +1,4 @@
-import { useQuery, gql } from 'gr';
 import icons from '~/assets/data/icons.json';
-
-export const GET_SETTINGS = gql`
-  query getSettings {
-    Settings: settings {
-      appLogo
-    }
-  }
-`;
 
 export default {
   name: 'AppIcon',
@@ -17,29 +8,11 @@ export default {
     name: {
       type: String,
       required: true,
-      validator: val => val in icons || val === 'appLogo',
+      validator: val => val in icons,
     },
   },
   render(createElement, { props }) {
-    const { result, loading, error } = useQuery(GET_SETTINGS);
-
     let icon = icons[props.name];
-
-    // If the icon is 'appLogo', wait for the GraphQL query to fetch the path
-    if (props.name === 'appLogo' && result.value) {
-      const settings = result.value.Settings;
-      if (settings && settings.appLogo) {
-        icon = {
-          path: settings.appLogo,
-          viewBox: '0 0 24 24',
-        };
-      }
-    }
-
-    if (!icon) {
-      return null; // Or some fallback element
-    }
-
     if (typeof icon === 'string') {
       icon = {
         path: icon,
@@ -82,3 +55,4 @@ export default {
     );
   },
 };
+
